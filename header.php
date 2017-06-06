8deb88e86dc9190410805877cf121e72ad424695
RV <RV@DESKTOP-U5CPECK>
2017-06-06 16:18:53 +0200

    debut

---
+++ header.php
@@ -0,0 +1,32 @@
+<header>
+    <div class ="container">
+        <div class="row">
+            <div class="col-xs-12  visible-xs"> <img class="img-responsive" src="img/headerXs.png" alt="header" style="width: 100%"> </div>
+            <div class="col-sm-12 visible-sm "> <img class="img-responsive" src="img/headerSm.png" alt="header" style="width: 100%"> </div>
+            <div class="col-md-12 visible-md"><img class="img-responsive" src="img/headerMd.png" alt="header" style="width: 100%"></div>
+            <div class="col-lg-9 visible-lg"><img class="img-responsive" src="img/headerLgetMd.png" alt="header" style="width: 100%"> </div>
+
+        </div>
+    </div>
+
+
+<!--            <div> <span class="col-lg-12 visible-lg alert alert-danger">ecran large</span> <span class="col-md-12 visible-md alert alert-warning">ecran medium</span> <span class="col-sm-12 visible-sm alert alert-warning">ecran small</span> <span class="col-xs-12 visible-xs alert alert-success">ecran extra small</span> </div>-->
+    <!--    HEADER-->
+
+    <div class="container">
+        <nav class="navbar navbar-default">
+            <div class="container">
+                <ul class="nav navbar-nav">
+                    <li <?php if (isset($_GET['p']) && $_GET['p'] == 1) echo $active ?>><a href="?p=1">Accueil</a></li>
+                    <li <?php if (isset($_GET['p']) && $_GET['p'] == 2) echo $active ?>><a href="?p=2">Présentation</a></li>                   
+                    <li <?php if (isset($_GET['p']) && $_GET['p'] == 6) echo $active ?>><a href="?p=6">HTML/CSS</a></li>
+                    <li <?php if (isset($_GET['p']) && $_GET['p'] == 7) echo $active ?>><a href="?p=7">Javascript/JQuery</a></li>
+                    <li <?php if (isset($_GET['p']) && $_GET['p'] == 8) echo $active ?>><a href="?p=8">PHP</a></li>
+                    <li <?php if (isset($_GET['p']) && $_GET['p'] == 9) echo $active ?>><a href="?p=9">Analyse/Conception</a></li>
+                    <li <?php if (isset($_GET['p']) && $_GET['p'] == 10) echo $active ?>><a href="?p=10">Symfony 2</a></li>
+
+                </ul>
+            </div>
+        </nav>
+    </div>
+</header>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+require('head.php');  //declaration doctype et balise meta 
+require('header.php');     //entete avec logo menu etc (commun a l'ensemble des pages)  
+require ('controleur.php');
+require('footer.php');
+?>
+
